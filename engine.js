8adb627eb512af97a192780dc4d7c589c0d1fab8
Devjiro76 <devjiro76@gmail.com>
2018-08-29 01:07:12 +0900

    ✨ 기능: 이슈 자동 클로즈 되도록
    WIP 제외하고 이슈 자동 클로즈 키워드 추가
    
    #1

--- engine.js
+++ engine.js
@@ -1,32 +1,37 @@
 "format cjs";
 
-var wrap = require('word-wrap');
-var map = require('lodash.map');
-var longest = require('longest');
-var rightPad = require('right-pad');
+var wrap = require("word-wrap");
+var map = require("lodash.map");
+var longest = require("longest");
+var rightPad = require("right-pad");
 
-var filter = function(array) {
+var filter = function(close, array) {
   return array.filter(function(x) {
-    return x;
+    return close ? "close " + x : x;
   });
 };
 
 // This can be any kind of SystemJS compatible module.
 // We use Commonjs here, but ES6 or AMD would do just
 // fine.
-module.exports = function (options) {
-
+module.exports = function(options) {
   var types = options.types;
-  var format = options.format || '[emoji] [name]: [subject]';
+  var format = options.format || "[emoji] [name]: [subject]";
 
   var length = longest(Object.keys(types)).length + 2;
-  var choices = map(types, function (type, key) {
+  var choices = map(types, function(type, key) {
     var name = type.name || key;
     return {
-      name: type.emoji + '  ' + rightPad(name + ':', length) + ' ' + type.description,
+      name:
+        type.emoji +
+        "  " +
+        rightPad(name + ":", length) +
+        " " +
+        type.description,
       value: {
         emoji: type.emoji,
         name: name,
+        close: type.close == undefined ? true : type.close
       }
     };
   });
@@ -53,39 +58,42 @@ module.exports = function (options) {
       // collection library if you prefer.
       cz.prompt([
         {
-          type: 'list',
-          name: 'type',
+          type: "list",
+          name: "type",
           // message: 'Select the type of change that you\'re committing:',
-          message: '어떤 타입의 커밋인가요?:',
+          message: "어떤 타입의 커밋인가요?:",
           choices: choices
-        }, {
-          type: 'input',
-          name: 'subject',
+        },
+        {
+          type: "input",
+          name: "subject",
           // message: 'Write a short, imperative tense description of the change:\n'
-          message: '변경 사항에 대해 짧은 제목을 지어주세요:\n'
-        }, {
-          type: 'input',
-          name: 'body',
-          message: '변경 사항에 대해 자세하게 설명해주세요:\n'
-        }, {
-          type: 'input',
-          name: 'issues',
+          message: "변경 사항에 대해 짧은 제목을 지어주세요:\n"
+        },
+        {
+          type: "input",
+          name: "body",
+          message: "변경 사항에 대해 자세하게 설명해주세요:\n"
+        },
+        {
+          type: "input",
+          name: "issues",
           // message: 'List any issues closed by this change:\n'
-          message: '이 커밋으로 해결처리 되는 이슈들(#1, #2,...):\n'
+          message: "이 커밋으로 해결처리 되는 이슈들(#1, #2,...):\n"
         }
       ]).then(function(answers) {
-
         var maxLineWidth = 100;
 
         var wrapOptions = {
           trim: true,
-          newline: '\n',
-          indent:'',
+          newline: "\n",
+          indent: "",
           width: maxLineWidth
         };
 
         // Hard limit this line
-        var head = format.replace(/\[emoji\]/g, answers.type.emoji)
+        var head = format
+          .replace(/\[emoji\]/g, answers.type.emoji)
           .replace(/\[name\]/g, answers.type.name)
           .replace(/\[subject\]/g, answers.subject.trim())
           .slice(0, maxLineWidth);
@@ -96,9 +104,9 @@ module.exports = function (options) {
 
         var issues = wrap(answers.issues, wrapOptions);
 
-        var footer = filter([ issues ]).join('\n\n');
+        var footer = filter(answers.type.close, [issues]).join("\n\n");
 
-        commit(head + '\n\n' + body + '\n\n' + footer);
+        commit(head + "\n\n" + body + "\n\n" + footer);
       });
     }
   };
